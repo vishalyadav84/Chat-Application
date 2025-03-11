2aceebab1184025b6844bc83ea99291b5d223cb8
Vishal Yadav <vishydv.to@gmail.com>
2025-03-11 11:55:11 +0530

    init

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,54 @@
+// const AuthImagePattern = ({ title, subtitle }) => {
+//   return (
+//     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+//       <div className="max-w-md text-center">
+//         <div className="grid grid-cols-3 gap-3 mb-8">
+//           {[...Array(9)].map((_, i) => (
+//             <div
+//               key={i}
+//               className={`aspect-square rounded-2xl bg-primary/10 ${
+//                 i % 2 === 0 ? "animate-pulse" : ""
+//               }`}
+//             />
+//           ))}
+//         </div>
+//         <h2 className="text-2xl font-bold mb-4">{title}</h2>
+//         <p className="text-base-content/60">{subtitle}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AuthImagePattern;
+
+const images = [
+  "/9.png",
+  "/2.png",
+  "/3.png",
+  "/4.png",
+  "/5.png",
+  "/6.png",
+  "/7.png",
+  "/8.png",
+  "/1.png",
+];
+
+const AuthImagePattern = ({ title, subtitle }) => {
+  return (
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+      <div className="max-w-md text-center">
+        <div className="grid grid-cols-3 gap-3 mb-8">
+          {images.map((src, i) => (
+            <div key={i} className="aspect-square rounded-2xl overflow-hidden">
+              <img src={src} alt={`Image ${i + 1}`} className="object-cover w-full h-full" />
+            </div>
+          ))}
+        </div>
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthImagePattern;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+
+const Navbar = () => {
+  const { logout, authUser } = useAuthStore();
+
+  return (
+    <header
+      className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+    backdrop-blur-lg bg-base-100/80"
+    >
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <MessageSquare className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">ChatApp</h1>
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Link
+              to={"/settings"}
+              className={`
+              btn btn-sm gap-2 transition-colors
+              
+              `}
+            >
+              <Settings className="w-4 h-4" />
+              <span className="hidden sm:inline">Settings</span>
+            </Link>
+
+            {authUser && (
+              <>
+                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
+
+                <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
+
+                
+                <Link to={"/profile"} className="flex items-center gap-2">
+                <div className="size-9 rounded-full bg-red-300 flex items-center justify-center text-white font-semibold">
+                  {authUser.fullName ? authUser.fullName.slice(0, 2).toUpperCase() : "U"}
+                </div>
+                <span className="hidden sm:inline font-medium">{authUser.fullName}</span>
+                </Link>                
+              </>
+            )}
+            
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Navbar;
