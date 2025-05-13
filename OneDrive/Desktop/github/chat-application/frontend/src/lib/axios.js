bac0ca20702a78421addd313371436ea7bc03ec0
Vishal Yadav <vishydv.to@gmail.com>
2025-05-14 01:14:05 +0530

    Updated dependencies and fixed vulnerabilities

--- OneDrive/Desktop/github/chat-application/frontend/src/lib/axios.js
+++ OneDrive/Desktop/github/chat-application/frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-1-nf7x.onrender.com/" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  // baseURL: import.meta.env.MODE === "development" ? "https://chat-application-1-nf7x.onrender.com/" : "/api",
   withCredentials: true,
 });
 
